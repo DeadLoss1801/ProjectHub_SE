1e104018a8e4fb830107ba65da5e9d5af0efaee3
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2024-04-07 12:04:46 +0530

    adding user-controller.js

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,60 @@
+const { User } = require('../model');
+
+// create a new user 
+const createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Read a user by ID
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// Update a user by ID
+const updateUserById = async (userId, userData) => async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+// Delete a user by ID
+const deleteUserById = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+module.exports = { createUser, getUserById, updateUserById, deleteUserById, getAllUsers };
--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { createUser, getUserById, updateUserById, deleteUserById, getAllUsers } = require('./controllers/user-controller');
 
-
+router.route('/user').post(createUser).get(getAllUsers)
+router.route('/user/:id').get(getUserById).put(updateUserById).delete(deleteUserById)
 module.exports = router;
